4648df744fb54c32835af7a7e3b6a74b249477ca
mauforonda <mauforonda@users.noreply.github.com>
2024-08-07 15:40:19 +0000

    deploy: 7d3b0c5d541037cb9a860cd49931922f20d64230

---
+++ _import/components/plot.ad6a058b.js
@@ -0,0 +1,160 @@
+import * as Plot from "../../_npm/@observablehq/plot@0.6.16/_esm.js";
+import { html, svg } from "../../_npm/htl@0.3.1/_esm.js";
+import { format } from "../../_npm/d3@7.9.0/_esm.js";
+
+function withGradient({ color, limit, id = "gradient" }, callback) {
+    return [
+        callback(`url(#${id})`),
+        () => svg`<defs>
+        <linearGradient id=${id} gradientTransform="rotate(90)">
+          <stop offset=0% stop-color=${color} stop-opacity=0.3 />
+          <stop offset=${limit}% stop-color=${color} stop-opacity=0 />
+        </linearGradient>
+      </defs>`,
+    ];
+}
+
+export function drawPlot(data, width) {
+    const colors = {
+        base: "#a3a3a3",
+        figures: "#34A853",
+    };
+
+    const [min, max] = data.reduce(
+        ([min, max], { median }) => [
+            Math.min(min, median),
+            Math.max(max, median),
+        ],
+        [Infinity, -Infinity]
+    );
+
+    const lowMin = data.reduce(
+        (lowLimit, { low }) => Math.min(lowLimit, low),
+        Infinity
+    );
+    const lowLimit = (max - min * 0.97) / (max - lowMin);
+
+    const scale = {
+        tickSize: 0,
+        label: null,
+    };
+
+    const dotLimit = {
+        x: "timestamp",
+        r: 0.5,
+        stroke: null,
+        fill: colors.base,
+    };
+
+    const dotMedian = {
+        x: "timestamp",
+        y: "median",
+    };
+
+    const wideMarks = [
+        Plot.ruleX(data, {
+            x: "timestamp",
+            y1: "low",
+            y2: "high",
+            strokeWidth: 0.2,
+            strokeDasharray: 2,
+        }),
+        Plot.dot(data, {
+            ...dotLimit,
+            y: "low",
+        }),
+        Plot.dot(data, {
+            ...dotLimit,
+            y: "high",
+        }),
+    ];
+
+    return Plot.plot({
+        marginTop: 20,
+        marginLeft: 5,
+        style: {
+            color: colors.base,
+        },
+        width,
+        x: {
+            ...scale,
+            type: "time",
+        },
+        y: {
+            ...scale,
+            axis: "right",
+            domain: [min * 0.97, max * 1.03],
+        },
+        marks: [
+            Plot.gridY({}),
+            width > 900 ? wideMarks : null,
+            withGradient(
+                { color: colors.figures, limit: (lowLimit * 100) / 2 },
+                (fill) =>
+                    Plot.areaY(data, {
+                        ...dotMedian,
+                        fill,
+                        curve: "basis",
+                    })
+            ),
+            Plot.line(data, {
+                ...dotMedian,
+                curve: "basis",
+                stroke: colors.figures,
+                strokeWidth: 0.8,
+                sort: "timestamp",
+            }),
+            Plot.dot(
+                data,
+                Plot.pointerX({
+                    ...dotMedian,
+                    r: "offers",
+                    fill: colors.figures,
+                    fillOpacity: 1,
+                    stroke: colors.figures,
+                    strokeWidth: 10,
+                    strokeOpacity: 0.2,
+                })
+            ),
+            Plot.dot(data, {
+                ...dotMedian,
+                r: 2,
+                fill: colors.figures,
+                fillOpacity: 0.9,
+                stroke: null,
+            }),
+        ],
+    });
+}
+
+export function displayObservation(observation) {
+    const numberFormat = format(".2f");
+    const timeFormat = Intl.DateTimeFormat("es-BO", {
+        month: "long",
+        year: "numeric",
+        day: "numeric",
+        hour: "numeric",
+        hour12: false,
+        minute: "2-digit",
+    });
+    return html`<div class="observation">
+        <div class="price">
+            <div class="limit">
+                <div>${numberFormat(observation.low)}</div>
+                <div class="annotation">desde</div>
+            </div>
+            <div class="median">
+                <div class="medianValue">
+                    <div class="currency">Bs.</div>
+                    <div class="value">${numberFormat(observation.median)}</div>
+                </div>
+                <div class="annotation">por 1 Dólar</div>
+            </div>
+            <div class="limit">
+                <div>${numberFormat(observation.high)}</div>
+                <div class="annotation">hasta</div>
+            </div>
+        </div>
+        <div class="date">${timeFormat.format(observation.timestamp)}</div>
+    </div>`;
+}
